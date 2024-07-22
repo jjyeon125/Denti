adeba32c40c566e02b4d75282b47ec959a39a1d4
jjyeon125 <129151550+jjyeon125@users.noreply.github.com>
2024-07-22 22:35:52 +0900

    first

---
+++ demo/demo/src/main/java/com/example/demo/FastTeethApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastTeethApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastTeethApplication.class, args);
+	}
+
+}
