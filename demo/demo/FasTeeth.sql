5bbba04b1dd07caba930c1efb001a6d06a7485d6
Gyoseung Ku <ku@gyoseung.me>
2024-08-01 23:50:05 +0900

    데이터베이스 외부에 구축
    그냥 실행하시면 됩니다ㅏㅏ

--- demo/demo/FasTeeth.sql
+++ demo/demo/FasTeeth.sql
@@ -1,15 +1,15 @@
 use FasTeeth;
 
-CREATE TABLE users (
-    user_no INT PRIMARY KEY AUTO_INCREMENT,
-    user_id VARCHAR(255) NOT NULL UNIQUE,
-    user_name VARCHAR(255) NOT NULL,
-    user_pwd VARCHAR(255) NOT NULL,
-    user_birth DATE NOT NULL,
-    user_phone VARCHAR(255),
-    user_mail VARCHAR(255),
-    gender VARCHAR(255)
-) ENGINE=INNODB;
+    CREATE TABLE users (
+        user_no INT PRIMARY KEY AUTO_INCREMENT,
+        user_id VARCHAR(255) NOT NULL UNIQUE,
+        user_name VARCHAR(255) NOT NULL,
+        user_pwd VARCHAR(255) NOT NULL,
+        user_birth DATE NOT NULL,
+        user_phone VARCHAR(255),
+        user_mail VARCHAR(255),
+        gender VARCHAR(255)
+    ) ENGINE=INNODB;
 
 CREATE TABLE Dentist (
     doc_no INT PRIMARY KEY AUTO_INCREMENT,
